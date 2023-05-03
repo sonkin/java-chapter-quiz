db7230649e30a9bcc36dd98894b60420348e9a70
vladson <vladson@ya.ru>
2023-05-03 12:16:54 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ const Main = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "https://jquiz.vercel.app",
+          "Access-Control-Allow-Origin": "https://jquiz.vercel.app/",
         },
         body: JSON.stringify(results),
       });
